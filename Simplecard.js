1b8b34fff73fa6d9c3408113dc7772fef365c572
Owusua22 <108684372+Owusua22@users.noreply.github.com>
2022-10-26 00:42:01 +0000

    Add files via upload

---
+++ Simplecard.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Simplecard =() => {
+    
+
+ return (
+       <div className="container">
+       <div className="row">
+       <div className="col-md-6">
+          
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS146NX74iEotw8sNDhpQ_ZIAJckaCouD3jJg&usqp=CAU" className="img-responsive"></img>
+       </div>
+       <div className="col-md-6">  
+       <h3>My Favorite Brand of Car</h3>
+       <p>Benz is my favourite car. It is my favorite car because of the prestige attached to it.My prefered model is the AMG brand. I like the white colour.</p>
+       </div>
+       
+       
+       </div>
+
+        
+        </div>
+        
+ )
+        
+        
+}
+export default Simplecard;
